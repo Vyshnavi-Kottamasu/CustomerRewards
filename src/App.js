d7a19941254d7f70d5706c185e8a0cf395a60ab6
Vyshnavi <kvyshnavi.98@gmail.com>
2025-01-01 18:40:39 +0530

    Added test cases and few code modifications

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from 'react';
-import RewardsList from './RewardsList'
+import RewardsList from './components/RewardsList/RewardsList'
 
 function App() {
   return (
